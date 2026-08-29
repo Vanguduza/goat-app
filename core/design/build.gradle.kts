4b881a77e6180cd97cd3d306414dd440fff1fdcd
Vanguduza <tapiwaguduza@gmail.com>
2026-08-29 23:13:33 +0200

    Add Farm OS design system module

---
+++ core/design/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.farmos.core.design"
+    compileSdk = 37
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+}
+
+kotlin { jvmToolchain(17) }
+
+dependencies {
+    api(platform("androidx.compose:compose-bom:2026.08.00"))
+    api("androidx.compose.material3:material3")
+    api("androidx.compose.foundation:foundation")
+    api("androidx.compose.runtime:runtime")
+}
