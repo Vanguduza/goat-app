eabe75d98c644f9c272b2a7e46590917466c01eb
Vanguduza <tapiwaguduza@gmail.com>
2026-08-29 23:26:33 +0200

    Migrate Room codegen to KSP under built-in Kotlin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -12,10 +11,14 @@ android {
 
 kotlin { jvmToolchain(17) }
 
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
+
 dependencies {
     api(project(":core:model"))
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
-    kapt("androidx.room:room-compiler:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
     testImplementation(kotlin("test"))
 }
