957f3c3181ff92302960740c7c87c4fea4417fee
Vanguduza <tapiwaguduza@gmail.com>
2026-08-29 23:24:09 +0200

    Keep core database species-neutral

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
-    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -15,10 +14,8 @@ kotlin { jvmToolchain(17) }
 
 dependencies {
     api(project(":core:model"))
-    implementation(project(":domain:goat"))
     implementation("androidx.room:room-runtime:2.8.4")
     implementation("androidx.room:room-ktx:2.8.4")
     kapt("androidx.room:room-compiler:2.8.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     testImplementation(kotlin("test"))
 }
