393a0099db7e49d424500ca0255274f89612ad60
Vanguduza <tapiwaguduza@gmail.com>
2026-08-29 23:12:28 +0200

    Add Supabase command transport module

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    namespace = "com.farmos.core.network"
+    compileSdk = 37
+    defaultConfig { minSdk = 26 }
+}
+
+kotlin { jvmToolchain(17) }
+
+dependencies {
+    api(project(":core:model"))
+    implementation("io.ktor:ktor-client-core:3.0.3")
+    implementation("io.ktor:ktor-client-okhttp:3.0.3")
+    implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+}
