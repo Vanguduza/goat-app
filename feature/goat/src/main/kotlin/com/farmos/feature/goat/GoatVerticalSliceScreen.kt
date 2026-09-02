8bf2ceb9a39591f2bbe6c38d4a33ee0f9c039e1c
Vanguduza <tapiwaguduza@gmail.com>
2026-09-02 23:11:12 +0200

    Fix Compose keyboard options import

--- feature/goat/src/main/kotlin/com/farmos/feature/goat/GoatVerticalSliceScreen.kt
+++ feature/goat/src/main/kotlin/com/farmos/feature/goat/GoatVerticalSliceScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
@@ -22,7 +23,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import com.farmos.core.design.FosDimens
 import com.farmos.domain.goat.GoatSearchResult
