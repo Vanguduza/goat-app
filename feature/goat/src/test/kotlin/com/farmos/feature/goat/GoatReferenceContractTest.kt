ca9178a946749e6bc3dd53688afec271b6bd459e
Vanguduza <215922244+Vanguduza@users.noreply.github.com>
2026-09-25 00:03:32 +0200

    fix: compile reference interaction contracts

--- feature/goat/src/test/kotlin/com/farmos/feature/goat/GoatReferenceContractTest.kt
+++ feature/goat/src/test/kotlin/com/farmos/feature/goat/GoatReferenceContractTest.kt
@@ -5,8 +5,8 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodes
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
