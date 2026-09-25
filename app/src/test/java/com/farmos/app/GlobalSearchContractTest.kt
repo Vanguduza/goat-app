14b09ecf5f550b8fc97b424c219e3ebf49033ce1
Vanguduza <215922244+Vanguduza@users.noreply.github.com>
2026-09-25 15:51:53 +0200

    test: use supported compose node query API

--- app/src/test/java/com/farmos/app/GlobalSearchContractTest.kt
+++ app/src/test/java/com/farmos/app/GlobalSearchContractTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.v2.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
